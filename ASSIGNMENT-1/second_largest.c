e576e655ef79024db7b1c0fc2b6f8ef6f5ff0147
srmohib92-ai <sr.mohib92@gmail.com>
2025-11-10 11:09:10 +0530

    Create second_largest.c

---
+++ ASSIGNMENT-1/second_largest.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int first = arr[0], second = -99999;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] > first) {
+            second = first;
+            first = arr[i];
+        } else if (arr[i] > second && arr[i] < first) {
+            second = arr[i];
+        }
+    }
+
+    printf("Second largest element: %d\n", second);
+    return 0;
+}
